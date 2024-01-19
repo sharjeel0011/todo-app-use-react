6ff67db6385562f6b5fd153b5494ecfc0b815e2f
sharjeel0011 <caryzoon@gmail.com>
2024-01-19 11:26:47 +0500

    first commit

---
+++ todos/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  function addTodo(event) {
+    event.preventDefault();
+    console.log(text);
+    todo.push(text)
+    setTodo([...todo]);
+    console.log(todo);
+    setText("")
+  }
+
+  return (
+    <>
+      <h1>Todo App </h1>
+      {/* <h3>{text}</h3> */}
+      <form onSubmit={addTodo}> 
+        <input type="text" placeholder="enter text" onChange={(e) => setText(e.target.value)} value={text} />
+        <button type="submit">addTodo</button>
+      </form>
+      <ul>
+        {todo.map((item, index) => {
+          return <li key={index}>{item}
+            <button onClick={()=>deleteTodo(index)}>delete</button>
+            <button>edit</button>
+          </li>
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default App
